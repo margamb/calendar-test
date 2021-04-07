0fa9542c0c659788def391ca96e66b8a9cae2d80
margamb <margamarben@gmail.com>
2021-04-07 17:18:21 +0200

    Add styles to infoDetailEvent component

--- src/components/InfoDetailEvent.js
+++ src/components/InfoDetailEvent.js
@@ -19,19 +19,48 @@ const InfoDetailEvent = () => {
 
   console.log('🐷', event);
   const age = event.age ? event.age : 'este evento no tiene edad recomenda';
+  const city = event.city ? event.city : 'no se sabe la ciudad';
+  const address = event.address ? event.address : 'no se sabe la direccion';
+  const information = event.information
+    ? event.information
+    : 'No se ha añadido ninguna informacion extra';
+  const link = event.link ? event.link : 'no hay link del evento';
+  const date = event.date ? event.date : 'no hay fecha del evento';
+  const hour = event.hour ? event.hour : '- -';
   return (
     <>
-      <div>
-        <h1>{event.name}</h1>
+      <div className="information-detail">
         <img className="image" src={event.image} />
-        <p>{age}</p>
-        <p>{event.city}</p>
-        <p>{event.address}</p>
-        <p>{event.information}</p>
-        <p>{event.link}</p>
-        <p>{event.date}</p>
-        <p>{event.hour}</p>
+        <div className="information-detail-text">
+          <h1 className="information-detail-title">{event.name}</h1>
+          <p className="information-detail-items">
+            <span className="title-items ">Fecha: </span>
+            {date}, {hour} horas
+          </p>
+          <p className="information-detail-items">
+            <span className="title-items">Ciudad: </span>
+            {city}
+          </p>
+          <p className="information-detail-items">
+            <span className="title-items">Direccion: </span>
+            {address}
+          </p>
+          <p className="information-detail-items">
+            <span className="title-items">Edad recomendada: </span>
+            {age} años
+          </p>
+          <p className="information-detail-items">
+            <span className="title-items">Informacion adicional: </span>
+            {information}
+          </p>
+          <p className="information-detail-items">
+            <span className="title-items">Link: </span>
+            {link}
+          </p>
+          <div className="cat-info"></div>
+        </div>
       </div>
+
       {/* <pre>
         <code>{JSON.stringify(event)}</code>
       </pre> */}
